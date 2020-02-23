ccb069e7d8affc0b1257c5005f6026672f9d1b72
andreagrioni <grioni.andrea@gmail.com>
2020-02-23 08:58:55 +0000

    add pybedtools to extract reads over BED regions

--- scripts/RegionSelection.py
+++ scripts/RegionSelection.py
@@ -1,55 +1,37 @@
-#!/usr/bin/python
-
 import argparse
-import datetime
-from EnsemblRestClient import EnsemblRestClient
-import sys
-import re
+from pybedtools import BedTool
 
-parser = argparse.ArgumentParser()
-parser = argparse.ArgumentParser(description='Put here a description.')
-parser.add_argument('-b', '--bed', type=str, help='Output bed file', required=True)
-parser.add_argument('-r', '--region', type=str, help='List of genes or regions to select from the bam-file', required=True)
 
-args = parser.parse_args()
+def get_arguments():
+    '''parse input arguments'''
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='NanoFG helper', description='run pybedtools to intersect a/b arbitrary files (bed/bam)')
+    parser.add_argument('-a', '--a_file', type=str, dest='a', help='bed/bam input 1', required=True)
+    parser.add_argument('-b', '--b_file', type=str, dest='b',help='bed/bam input 2', required=True)
+    parser.add_argument('-o', '--output_name', type=str, dest='fo',help='file output name/path', required=True)
+    args = parser.parse_args()
+    return args
+    
 
-bed=args.bed
-selection=args.region
-selection=selection.split(",")
-EnsemblRestClient=EnsemblRestClient()
+def intersect(a,b):
+    '''intersect a and b input files
+    with pybedtools
+    
+    arguments:
+    a=BedTool Object
+    b=BedTool Object
 
-### Create a bed-file that contains all the regions that need to be selected from the full bam-file
-with open(bed, "w") as bed:
-    for item in selection:
+    return:
+    BedTool Object
+    '''
+    a=BedTool(a)
+    b=BedTool(b)
+    a_b=a.intersect(b)
+    return a_b
 
-        ### If a region is given in a chr:pos1-pos2 format, this format is directly used
-        if re.match(".+:\d+-\d+", item):
-            chrom=item.split(":")[0]
-            pos1=item.split(":")[1].split("-")[0]
-            pos2=item.split(":")[1].split("-")[1]
 
-        else:
-            server='http://grch37.rest.ensembl.org'
-
-            ### If a desired region is given by a ensembl identifier (e.g. ENSG00000141510) or the gene name, the positions are taken from ensembl
-            if re.match("^ENS", item):
-                endpoint="/lookup/id/"+item
-            else:
-                endpoint="/lookup/symbol/homo_sapiens/"+item
-
-            headers={"Content-Type" : "application/json"}
-            params={"expand": "1"}
-
-            ensembl_request=EnsemblRestClient.perform_rest_action(server, endpoint, headers, params)
-            chrom=str(ensembl_request["seq_region_name"])
-            pos1=str(ensembl_request["start"])
-            pos2=str(ensembl_request["end"])
-
-        try:
-            if pos1<pos2:
-                bed.write("\t".join([chrom, pos1, pos2])+"\n")
-            else:
-                bed.write("\t".join([chrom, pos2, pos1])+"\n")
-        except:
-            ### If pos1 or pos2 does not exist, the given region is not correct or the gene cannot be found.
-            sys.exit(item+" is not a valid selection. Use a valid ensembl id, gene name or region (e.g. 3:1234-2345)")
+if __name__ == '__main__':
+    args = get_arguments()
+    intersected = intersect(args.a, args.b)
+    # save to fo
+    intersected.saveas(args.fo)
